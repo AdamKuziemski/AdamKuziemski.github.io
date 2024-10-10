ae221b486b29704f818d4dceee9c9308cbb06146
Adam Kuziemski <kuziemski.a@gmail.com>
2024-10-10 14:30:09 +0200

    add printing truth tables

--- logic.js
+++ logic.js
@@ -5,13 +5,35 @@ class LogicCheck {
   #generateTruthTable = (n) => Array.from(Array(2 ** n)).map(
     (_, i) => Array.from(Array(n)).map((_, j) => !!(i & (2 ** j)))
   );
+
+  #printTruthTable = (predicates, propositionalVariables, results) => {
+    console.table(predicates.map((p, i) => {
+      const predicateBody = p.toString();
+      const varsStart = predicateBody.indexOf('(');
+      const varsEnd = predicateBody.indexOf(')');
+      const variables = predicateBody.slice(varsStart + 1, varsEnd).split(',').map(v => v.trim());
+      const row = variables.reduce((rowData, v, k) => {
+        rowData[v] = propositionalVariables[k];
+        return rowData;
+      }, {});
+      row.predicate = predicateBody;
+      row.result = results[i];
+      return row;
+    }));
+  };
+
+  #forAllPredicates = (predicates, propositionalVariables, printTable) => {
+    const results = predicates.map(predicate => predicate(...propositionalVariables));
+
+    if (printTable) {
+      this.#printTruthTable(predicates, propositionalVariables, results);
+    }
+
+    return results;
+  }
   
-  #forAllPredicates = (predicates, propositionalVariables) => predicates.map(
-    predicate => predicate(...propositionalVariables)
-  );
-  
-  isTheSameReasoning = (predicates) => this.#generateTruthTable(this.#parameterLength(predicates))
-    .every((row) => this.#hasIdenticalValues(this.#forAllPredicates(predicates, row)));
+  isTheSameReasoning = (predicates, printTable = false) => this.#generateTruthTable(this.#parameterLength(predicates))
+    .every((row) => this.#hasIdenticalValues(this.#forAllPredicates(predicates, row, printTable)));
 }
 
 const logos = new LogicCheck();
